ee95f3f9543ba5fb95ce7a6a92044914614a09dc
Izzatullokh24 <86156093+Izzatullokh24@users.noreply.github.com>
2023-04-14 21:36:08 +0900

    Add files via upload

---
+++ arithmetic operators.m
@@ -0,0 +1,10 @@
+#this script computes the area of the and circumference od circle
+b=17.2 
+secondprob = nthroot(b + 9.8,3);
+a=4;
+third  = 10 * (5 * a + 16)^(1/2)
+output1 = (b + 9.8)^(1/3)
+disp("third = "); disp(third)
+disp("output1 = "); disp(output1)
+output2 = 10*sqrt(5*a + 16);
+disp("output2 = "); disp(output2)
